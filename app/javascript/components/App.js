941baad1184e7de85cfad3942be003c5f02e82fb
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-05 02:34:07 +0300

    Add react component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,21 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import store from "../configureStore.js";
+import Greeting from "./greetings.js";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HelloWorld from './HelloWorld'
- 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-          <Routes>
-            <Route exact path="/" render = {() => ("Home!")} />
-            <Route path="/hello" render= {() => <HelloWorld greeting="Friend"/>} />
-          </Routes>
-      </BrowserRouter>
+    <Provider store={store}>
+     <BrowserRouter>
+        <Routes>
+        <Route path="/" element={ <Greeting /> } />
+        </Routes>
+     </BrowserRouter>
+     </Provider> 
     );
   }
 }
 
 export default App
-
