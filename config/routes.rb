3d9f2fee4709e645163bab9909db83d655a293b8
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-04 23:24:37 +0300

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace defaults: { formart: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
 end
